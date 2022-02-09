74f444cfc5880c258e6e65758c677352e76dac97
Gagandatt <gagandattrewala.com>
2022-02-09 17:31:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from './Components/Navbar'
+import Hero from './Components/Hero'
+import Static from './Components/Static'
+import Test from './Components/Test'
+import Footer from './Components/Footer'
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Hero/>
+    <Static/>
+    <Test/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
